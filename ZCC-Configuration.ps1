bcc243b3bc9a99d61d29ec4fac868c40b6097d4b
Rizwan Shaikh <122839389+shaikhr1@users.noreply.github.com>
2023-01-17 13:55:04 -0500

    Add files via upload

---
+++ ZCC-Configuration.ps1
@@ -0,0 +1,11 @@
+﻿# Define the Zscaler tenant and user credentials
+$tenant = "mytenant"
+$username = "myusername"
+$password = "mypassword"
+
+# Install the Zscaler client
+Invoke-Expression (Get-Content "path\to\installer.exe" -Raw)
+
+# Configure the Zscaler client with the tenant and user credentials
+$zscaler = New-Object -ComObject "Zscaler.ZscalerComWrapper"
+$zscaler.SetUpZscalerService($tenant, $username, $password)
